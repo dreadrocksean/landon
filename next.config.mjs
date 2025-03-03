b2c91d30365d8f5c76978f621cc22dc19036578e
Adrian Bartholomew <adrian@bartholomusic.com>
2025-03-03 16:50:10 -0600

    Add yaml files

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: "imgix", // or use 'cloudinary', 'akamai', or other supported loaders
+    path: "", // or specify a custom path if needed,
+    unoptimized: true, // Disables the default behavior of optimizing images with the next/image component
+    // disableStaticImages: true, // Disable the default image optimization
+  },
   output: "export",
 };
 
