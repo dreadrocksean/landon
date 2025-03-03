bad14560251925f8e8c5f395afdc3ba7eeb4f583
Adrian Bartholomew <adrian@bartholomusic.com>
2025-03-03 16:52:05 -0600

    random edit

--- components/featured.jsx
+++ components/featured.jsx
@@ -35,7 +35,7 @@ const Featured = () => {
               everywhere he performs. He's not awesome "for his age". He's
               simply awesome! Performing professionally since he was 10, his
               confidence and audience participation defies his years. He's a
-              cutie too!
+              cutie too!!
             </p>
             <Link
               className="font-medium text-rose hover:text-white duration-300 transition-all ease-linear"
