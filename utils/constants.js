ea2bd902912b9241426fc70c2620386392dd0abb
Adrian Bartholomew <adrian@bartholomusic.com>
2025-03-05 10:10:01 -0600

    Add menu item

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,6 @@
 export const navigationLinks = [
   { id: 1, title: "Home", route: "/" },
+  { id: 2, title: "about", route: "#featured_music" },
   // { id: 3, title: "albums", route: "#albums" },
   { id: 5, title: "gallery", route: "#gallery" },
   { id: 7, title: "Shows", route: "#shows" },
