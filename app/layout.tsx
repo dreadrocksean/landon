d518aa54f073dd7dc98e696f5a6e0eecf810d7c0
Adrian Bartholomew <adrian@bartholomusic.com>
2025-07-17 09:18:22 -0500

    fix layout format re nextjs requirements

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 import "aos/dist/aos.css"; // Import AOS CSS file
+
 // Fonts from google fonts
 import {
   dMSans,
@@ -9,43 +10,45 @@ import {
   pacifico,
   poppins,
 } from "@/fonts/fonts";
+
 import AnimationProvider from "./AnimationProvider";
 import Settings from "@/components/Settings";
 import LayoutProvider from "./LayoutProvider";
 import "@/styles/layout.css";
 
-// Homepage Metadata
+// ✅ Required metadata export in App Router
 export const metadata = {
   title: "Landon Music - Music website",
   description:
     "Landon Music is a great Nextjs Template Music Web designed to accelerate his online presence",
 };
 
-interface RootLayoutProps {
+// ✅ Required RootLayout type
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          spaceGrotesk.className,
+          dMSans.variable,
+          kumbhSans.variable,
+          pacifico.variable,
+          poppins.variable
+        )}
+      >
+        <AnimationProvider>
+          <LayoutProvider>
+            <>
+              <Settings />
+              {children}
+            </>
+          </LayoutProvider>
+        </AnimationProvider>
+      </body>
+    </html>
+  );
 }
-
-export const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
-  <html lang="en">
-    <body
-      className={twMerge(
-        spaceGrotesk.className,
-        dMSans.variable,
-        kumbhSans.variable,
-        pacifico.variable,
-        poppins.variable
-      )}
-    >
-      <AnimationProvider>
-        <LayoutProvider>
-          <>
-            <Settings />
-            {children}
-          </>
-        </LayoutProvider>
-      </AnimationProvider>
-    </body>
-  </html>
-);
-
-export default RootLayout;
--- lib/gcp/options.ts
+++ lib/gcp/options.ts
@@ -1,24 +1,33 @@
 import { ServiceAccount } from "firebase-admin";
 import { FirebaseOptions } from "firebase/app";
 
+// export const firebaseConfig: FirebaseOptions = {
+//   apiKey: "AIzaSyB7KeJNbcyroHBp564KN8liPXMN1e_sTP4",
+//   authDomain: "tellmewhattoplay-1.firebaseapp.com",
+//   databaseURL: "https://tellmewhattoplay-1.firebaseio.com",
+//   projectId: "tellmewhattoplay-1",
+//   storageBucket: "tellmewhattoplay-1.appspot.com",
+//   messagingSenderId: "177859591611",
+//   appId: "1:177859591611:web:04d7a35bee4048d0",
+// };
+
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyB7KeJNbcyroHBp564KN8liPXMN1e_sTP4",
-  authDomain: "tellmewhattoplay-1.firebaseapp.com",
-  databaseURL: "https://tellmewhattoplay-1.firebaseio.com",
-  projectId: "tellmewhattoplay-1",
-  storageBucket: "tellmewhattoplay-1.appspot.com",
-  messagingSenderId: "177859591611",
-  appId: "1:177859591611:web:04d7a35bee4048d0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
 export const serviceAccount: ServiceAccount = {
-  // type: "service_account",
-  projectId: "tellmewhattoplay-1",
-  // privateKeyId: "9d7e7c662c2e9c1bdb00900d3dca407a6ef7eafe",
-  privateKey:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCfKYPz6dN426/9\n7HiPrELYM7/VBwAbCDLSHAULFFvp+xUjbZURMTqGzvHsIwRlp1ij6ExWIxb5hjjM\nimXXOG3D3+UBnMe975TEGA3F6+jjxzJ/O7S4SN0cKBCTPlpwNRwUkBD+O8TxMqGC\nKyv63fmZKfyDrcS0ghKVptmn0gUfqzQ7A2rAPmAiuR8TCOZ2OnMkxWEPayx/VTCS\nRo9DfNbYWPTcivtNvtwu79ih3JNeQJ7SzqZ5IVSarXIWv1Z47P/Cwz46+moQV68S\nI5F9Mx6bHIKAXD7W2WwzrZkzoROeEPyLcDZ/26RtetW/VDtrGoP6MHZa341dM/s1\nVAB8UpV5AgMBAAECggEABiduyLGT0ap85Y6NItEATKfDlRMON/1bKbCXqVQCMcvZ\nh2MnTmDe6HN7+ytnch7EWKIUwYfdzYwZCh+XzHiMFfTEQAm112w6DlL3xV1Xxebf\n1veSB0YZ9oq+CVNx38DN8yWW4p2hAcp5YDUXID4Q9pEfCGBhqLEW0t5+r/R46j5+\nzFoPytxBw9NiKJ2HN8WnuFs+88rIkh7FYJ6j6vhuG05FPbh+ESAgPIHOBSA7iSM2\nq4zU5ikpUY6wHiieZ9ko19fg1NwHHKVJ0CCeKvMHP5CinBTf7pvt70SAqSKgMGl3\ndGJeUeCNs4u3gwSUFGQNX8PcRd7XeQO4RAKMRSxmOQKBgQDK4ilak2obaf5oNEDF\nYBKvxESg4oD9abLWXc95+dTHzIPs6QoALrT5aE1gw7ukoakOBrvO5i+hlQAXW1a3\nHMmVqdJvTQ9KW0R+VWEMm0ScqyGItvY+b1x+VyW6oeFyPdRW18lbkjxGKufrb5Jm\nS+GBvk1XsZzLOdOmjwIya5lZdwKBgQDI1QbneDCLBwJxxL+o761e9LYpirGSmtpf\nHaGpbmUSGos6D52xnOXIcfh9OqSai2rueODjzIXbKG3xYJzloHnSqLqXFqVrNO76\nCk3nutCVef5kmZBuX2t/a4oZZzAKItkMj7RSUPGlX1L1wRlpiEbFowLWjkZ2Ll5i\nAy5q4spEjwKBgDBdP6x27dmBKAOhSTuEDpBWAmIz/LnsY5SUomwacW37xAtK9nZa\nrAu0oJWLs4SogQt18Xl+CCbFxVLMFFzsgOWrqR2dSiLYpPhU1CDUNsP+S60yqHrz\nwyHfFoBqLLs2IBNft4Gb18CGY+kNHOJD/osWWYupLI6Jx3myJ7zrR5A7AoGBALI4\nSS4z/HADwmmPfb2/JA02AEmRKM++17a8pu+iZdLQ92iI3xn07/0XFrqaxePXgQcr\nAfdlUgwQLrCbWeB0rmcBEMRvdlBS4xfE+SmBIQTlA5nmpMMtrkme9yhf58O29QIu\nrIi4CBkHJKmtuI1sP9G3PReFnoXV1DAKPrDfnTuFAoGAWTEOFzC8mwspvh0j+K0W\n/TvMuPuZGYOAN1TUmuEg4FBvVwxx1GNg3ZD4bZYIHDMutNlobr0Srts9zORUWG23\nBFXmzqUQ4iT7F+maNSyb92PvqMFzgMoTCOj1wlpS6N5kT7Djz2RJb4TV0qNGCXAQ\nFKKTMJAaKryDZWZoJulj40c=\n-----END PRIVATE KEY-----\n",
-  clientEmail:
-    "firebase-adminsdk-fbsvc@tellmewhattoplay-1.iam.gserviceaccount.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY!.replace(
+    /\\n/g,
+    "\n"
+  ),
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL!,
   // clientId: "104427249681064068693",
   // authUri: "https://accounts.google.com/o/oauth2/auth",
   // tokenUri: "https://oauth2.googleapis.com/token",
