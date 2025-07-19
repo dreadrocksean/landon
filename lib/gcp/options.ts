1648abe2a86b0480a73d51214b898fd3280a7495
Adrian Bartholomew <adrian@bartholomusic.com>
2025-07-19 00:34:14 -0500

    create new api key for this project on fb

--- lib/gcp/options.ts
+++ lib/gcp/options.ts
@@ -1,11 +1,12 @@
 import { FirebaseOptions } from "firebase/app";
 
 export const firebaseConfig: FirebaseOptions = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+  apiKey: "AIzaSyAWK-_hqsho7UvXAnNqSnlMzvDswJbHCEg",
+  authDomain: "tellmewhattoplay-1.firebaseapp.com",
+  databaseURL: "https://tellmewhattoplay-1.firebaseio.com",
+  projectId: "tellmewhattoplay-1",
+  storageBucket: "tellmewhattoplay-1.appspot.com",
+  messagingSenderId: "177859591611",
+  appId: "1:177859591611:web:9acecdf611c8872c52189e",
+  measurementId: "G-EHQ1186Y2J",
 };
